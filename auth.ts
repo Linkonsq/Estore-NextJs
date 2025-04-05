48aec202994279f99872bac21779e61734130f5c
Linkon Siddique <linkon.sq48@gmail.com>
2025-04-05 16:37:19 +0600

    Customize token with jwt callback

--- auth.ts
+++ auth.ts
@@ -69,6 +69,27 @@ export const config = {
 
       return session;
     },
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async jwt({ token, user }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+
+        // If user has no name then use the email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
+    },
   },
 } satisfies NextAuthConfig;
 
